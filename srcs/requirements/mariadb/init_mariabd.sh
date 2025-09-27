d23535d7783ef7fa8822f37d7d7c1f27ed83217b
lilith <jfranco@fu-r7-p5.s19.be>
2025-09-27 20:16:47 +0200

    ADD: is working

--- srcs/requirements/mariadb/init_mariabd.sh
+++ srcs/requirements/mariadb/init_mariabd.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+cat > /etc/mysql/my.cnf << EOF
+
+[mysqld]
+bind-address = 0.0.0.0
+port = 3306
+
+
+EOF
 
 echo "Start mariaDB"
 service mariadb start
-sleep 5
+
+sleep 6
+
 
 mariadb -u root << EOF
 CREATE DATABASE IF NOT EXISTS \`${NAME_DATABASE}\`;
@@ -14,7 +24,7 @@ GRANT ALL PRIVILEGES ON \`${NAME_DATABASE}\`.* TO '${SQL_USER}'@'%';
 
 FLUSH PRIVILEGES;
 EOF
-mariadb-admin -u root -p{$SQL_PASS} shutdown
+mariadb-admin -u root -p$SQL_PASS shutdown
 mariadbd-safe
 
 echo "Fine script"
--- srcs/requirements/wordpress/config_wordpress.sh
+++ srcs/requirements/wordpress/config_wordpress.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
+sleep 30
 echo "Start init wordpress"
 
 cat > /etc/wordpress/config-default.php << EOF
@@ -8,7 +9,7 @@ cat > /etc/wordpress/config-default.php << EOF
 define('DB_NAME','${NAME_DATABASE}');                // nome del database
 define('DB_USER','${SQL_USER}');                  // utente MariaDB
 define('DB_PASSWORD', '${SQL_PASS}');        // password MariaDB
-define('DB_HOST', 'mariadb:3306');                  // hostname o container name DB
+define('DB_HOST', 'mariadb');                  // hostname o container name DB
 
 // URL del sito
 define('WP_HOME', 'https://localhost:8080');
@@ -18,11 +19,25 @@ define('WP_SITEURL', 'https://localhost:8080');
 define('FS_METHOD', 'direct');
 define('WP_DEBUG', false);
 EOF
-sed -i 's/^;listen = .*/listen = wordpress:9000/' /etc/php/8.4/fpm/pool.d/www.conf
-sed -i 's/^;clear_env = .*/clear_env = no/' /etc/php/8.4/fpm/pool.d/www.conf
+#sed -i 's/^;listen = .*/listen = 0.0.0.0:9000/' /etc/php/8.4/fpm/pool.d/www.conf
+#sed -i 's/^;clear_env = .*/clear_env = no/' /etc/php/8.4/fpm/pool.d/www.conf
+cat >> /etc/php/8.4/fpm/pool.d/www.conf << EFO
+listen = 0.0.0.0:9000
+clear_env = no
+EFO
 cat /etc/wordpress/config-default.php 
 env
 wp core version  --path=/usr/share/wordpress --allow-root
 wp core install --allow-root --path=/usr/share/wordpress --url="https://localhost:8080" --title="test" --admin_user="admin" --admin_password="password" --admin_email="email@example.com"
 wp user create --allow-root bob bob@example.com --role=author --path=/usr/share/wordpress 
-service php8.4-fpm restart
+#service php8.4-fpm shutdown
+#killall php8.4-fpm
+sleep 1
+export HTTP_HOST="https://localhost:8080"
+echo "avvio php8.4"
+#root@9743c0077094:/# killall /usr/sbin/php-fpm8.4
+#root@9743c0077094:/# /usr/sbin/php-fpm8.4 -F
+killall /usr/sbin/php-fpm8.4
+/usr/sbin/php-fpm8.4 -F -R
+
+#php8.4-fpm -F -R
